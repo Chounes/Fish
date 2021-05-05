dd755a90c4db80bde3f710ed6fdc7aac8e1cd6d0
Chounes <maxride445@gmail.com>
2021-05-05 17:10:45 +0200

    Exo 5 OK/2 et 6 OK

--- fish/fish.c
+++ fish/fish.c
@@ -1,74 +1,13 @@
 #define _DEFAULT_SOURCE
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <signal.h>
+
 
 #include "cmdline.h"
-
+#include "fish.h"
 #define BUFLEN 1024
 
 #define YES_NO(i) ((i) ? "Y" : "N")
 
-/*Exercise 3
-Execute basic commands*/
-void exeSimpleCommand(struct line *li){
 
-  //If command have'nt any arguments
-  if(li->cmds->n_args <1){
-    fprintf(stderr,"Error : please enter a command\n");
-    fprintf(stderr,"Usage : [command] [options]\n");
-  }
-
-  //Don't execute command if command start by "exit" or "cd"
-  else if(strcmp(li->cmds[0].args[0],"exit") == 0 || strcmp(li->cmds[0].args[0],"cd") == 0);
-
-  else{
-    /*Exercise 6
-    Reset SIGINT to it default value just
-    for execution of command*/
-    struct sigaction dflt;
-    dflt.sa_flags = 0;
-    sigemptyset(&dflt.sa_mask);
-    dflt.sa_handler = SIG_DFL;
-    sigaction(SIGINT, &dflt, NULL);
-
-    //Execute command in sub process
-    if(fork()==0){
-      int res = execvp(li->cmds[0].args[0],li->cmds[0].args);
-
-      //If command badly executed, print error of command
-      if(res == -1){
-        perror(li->cmds[0].args[0]);
-      }
-      exit(1);
-    }
-    int stat;
-    //Wait end of sub process
-    int pid = wait(&stat);
-
-
-    //Print pid of sub process and informations about it execution
-    if(WIFEXITED(stat)){
-        printf("%d exited, status=%d\n", pid, WIFSIGNALED(stat));
-    }
-    if(WIFSIGNALED(stat)){
-        printf("%d killed by signal %d\n", pid, WTERMSIG(stat));
-    }
-
-    /*Exercise6
-    Re-ignore SIGINT for the main loop*/
-    dflt.sa_handler = SIG_IGN;
-    sigaction(SIGINT, &dflt, NULL);
-  }
-
-  return;
-}
 
 //void handler(int sig);
 
