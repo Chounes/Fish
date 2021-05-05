9a2dab2c92cba3a01dd90573bf97e6259534b433
Doudi05 <74101247+Doudi05@users.noreply.github.com>
2021-05-05 19:05:53 +0200

    Update fish.h

--- fish/fish.h
+++ fish/fish.h
@@ -16,6 +16,8 @@
 #define INPUT_REDIRECT 0
 #define OUTPUT_REDIRECT 1
 
+char *chabsolu = getcwd(NULL, 0);
+
 //Handle input and output redirection. Return the new file descriptor if succeeded, -1 if not
 int cmd_redirection(const char *file, int type)
 {
@@ -53,6 +55,54 @@ int cmd_redirection(const char *file, int type)
 	}
 
 	close(fd);
+
+	return 0;
+}
+
+//Handle internal commands like cd or exit. Return 1 if an internal command other than exit has been executed, -1 if more than 1 commands, 0 if no internal commands.
+int cmd_interne(struct line li)
+{
+	if(li.n_cmds > 1)
+	{
+		return -1;
+	}
+
+	//exit command
+	if(strcmp(li.cmds[0].args[0],"exit") == 0)
+	{
+		printf("exiting...\n");
+		line_reset(&li);
+		exit(EXIT_SUCCESS);
+	}
+
+	//cd command
+	if(strcmp(li.cmds[0].args[0],"cd") == 0)
+	{
+		size_t len_dir = strlen("home") + strlen(getenv("USER")) + 1;
+		char dir[len_dir];
+		for (size_t i = 0; i < len_dir; ++i) {
+			dir[i] = '\0';
+		}
+		if (li.cmds[0].n_args < 2) {
+		  	strcpy(dir, "~");
+		} else {
+			strcpy(dir, li.cmds[0].args[1]);
+		}
+		if (strcmp(dir, "~") == 0) {
+			char *user = getenv("USER");
+			strcpy(dir, "/home/");
+			strcat(dir, user);
+		}
+		if (chdir(dir) == -1) {
+			perror("chdir");
+			fprintf(stderr, "failed to change directory to %s\n", dir);
+			line_reset(&li);
+			continue;
+		}
+		free(chabsolu);
+		chabsolu = getcwd(NULL, 0);
+		return 1;
+	}
 
 	return 0;
 }
