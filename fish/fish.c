4f8117e10448242e14c651186c6003da8b797485
Doudi05 <74101247+Doudi05@users.noreply.github.com>
2021-05-20 01:08:16 +0200

    Update fish.c

--- fish/fish.c
+++ fish/fish.c
@@ -1,5 +1,12 @@
-#define _DEFAULT_SOURCE
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
 
 #include "cmdline.h"
 #include "fish.h"
