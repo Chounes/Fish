273562aa1a25284d60ded5d02e214eb484d6b487
Doudi05 <74101247+Doudi05@users.noreply.github.com>
2021-05-23 18:58:22 +0200

    Update fish.c

--- fish/fish.c
+++ fish/fish.c
@@ -1,51 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <signal.h>
+#define _DEFAULT_SOURCE
+
 
 #include "cmdline.h"
 #include "fish.h"
-#define BUFLEN 1024
+#define BUFLEN 4096
 
 #define YES_NO(i) ((i) ? "Y" : "N")
-
-
-
-//void handler(int sig);
-
-//struct list pids; // list of background processes pid
 
 int main() {
   //Exercise 6
   //Ignore signal SIGINT for the main loop
-  /*struct sigaction ignored;
+  struct sigaction ignored;
   ignored.sa_flags = 0;
   sigemptyset(&ignored.sa_mask);
   ignored.sa_handler = SIG_IGN;
-  sigaction(SIGINT, &ignored, NULL);*/
+  sigaction(SIGINT, &ignored, NULL);
 
-  struct sigaction ignored;
-	struct sigaction oldaction;//save the default action when SIGINT is received
-	sigemptyset(&ignored.sa_mask);
-	ignored.sa_handler = SIG_IGN;//ignoring SIGINT in main process
-	ignored.sa_flags = 0;
-	sigaction(SIGINT, &ignored, &oldaction);
+  //Default value to reset buffer at end of background command
+  char reset_buf[] = "\n\0";
+
 
   struct line li;
-  char buf[BUFLEN];
+  char buf[BUFLEN];//Buffer
+	char wd[BUFLEN];//Workdir
 
   line_init(&li);
 
-  char *chabsolu = getcwd(NULL, 0);
-  //int **pipes = NULL;
+  //char *chabsolu = getcwd(NULL, 0);
 
   for (;;) {
-    printf("fish> ");
+    printf("fish: %s> ",getcwd(wd,BUFLEN));
     fgets(buf, BUFLEN, stdin);
 
     int err = line_parse(&li, buf);
@@ -56,47 +40,26 @@ int main() {
     }
 
     /*Exercise 3
-    Execute simple commande*/
-    exeSimpleCommand(&li);
+    If commande entered*/
+    if(li.cmds->n_args != 0){
+      //Execute simple commande
+      //exeSimpleCommand(&li);
+      handle_with_pipes(li, ignored);
+    }
 
-
-    //potentiellement à enlever
     if (li.cmds[0].args[0] == NULL) {
       line_reset(&li);
       continue;
     }
 
-    //Exercise 4
-    cmd_interne(li, chabsolu);
+    cmd_interne(li);
 
-    fprintf(stderr, "Command line:\n");
-    fprintf(stderr, "\tNumber of commands: %zu\n", li.n_cmds);
-
-    for (size_t i = 0; i < li.n_cmds; ++i) {
-      fprintf(stderr, "\t\tCommand #%zu:\n", i);
-      fprintf(stderr, "\t\t\tNumber of args: %zu\n", li.cmds[i].n_args);
-      fprintf(stderr, "\t\t\tArgs:");
-      for (size_t j = 0; j < li.cmds[i].n_args; ++j) {
-        fprintf(stderr, " \"%s\"", li.cmds[i].args[j]);
-      }
-      fprintf(stderr, "\n");
+    if(li.background){
+      strcpy(buf,reset_buf);
     }
-
-    fprintf(stderr, "\tRedirection of input: %s\n", YES_NO(li.redirect_input));
-    if (li.redirect_input) {
-      fprintf(stderr, "\t\tFilename: '%s'\n", li.file_input);
-    }
-
-    fprintf(stderr, "\tRedirection of output: %s\n", YES_NO(li.redirect_output));
-    if (li.redirect_output) {
-      fprintf(stderr, "\t\tFilename: '%s'\n", li.file_output);
-    }
-
-    fprintf(stderr, "\tBackground: %s\n", YES_NO(li.background));
-
-    /* do something with li */
 
     line_reset(&li);
+
   }
 
   return 0;
